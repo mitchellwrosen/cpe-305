8aa708d3425402d0ecf90004595d9ab436b314bf
Mitchell Rosen <mitchellwrosen@gmail.com>
2012-10-02 15:20:22 -0700

    in->test

--- tests/gen.py
+++ tests/gen.py
@@ -5,14 +5,14 @@ import os
 for filename in os.listdir('.'):
   if filename.endswith('.gen'):
     genfile = open(filename)
-    infile = open(filename[:-4], 'w')
+    testfile = open(filename[:-4], 'w')
     for line in genfile:
       hash_index = line.find('#')
       if hash_index != -1:
         line = line[:hash_index] + '\n'
       if line.strip():
-        infile.write(line)
+        testfile.write(line)
 
     genfile.close()
-    infile.close()
+    testfile.close()
 
