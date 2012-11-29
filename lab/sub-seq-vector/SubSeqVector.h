b8b796ed95b7f20cc682ee1ecc349242e312e341
Mitchell Rosen <mitchellwrosen@gmail.com>
2012-11-29 01:28:26 -0800

    more work

--- lab/sub-seq-vector/SubSeqVector.h
+++ lab/sub-seq-vector/SubSeqVector.h
@@ -3,5 +3,9 @@
 
 template <typename T> class SubSeqVector : public std::vector<T> {
 public:
-   SubSeqVector SubSeq(int start, int end) { }
+   SubSeqVector() : std::vector<T>() { }
+   template <typename I> SubSeqVector(I a, I b) : std::vector<T>(a, b) { }
+   SubSeqVector SubSeq(int start, int end) {
+      return SubSeqVector(this->begin() + start, this->begin() + end);
+   }
 };
--- lab/sub-seq-vector/tests/SubSeqVectorTest.cpp
+++ lab/sub-seq-vector/tests/SubSeqVectorTest.cpp
@@ -2,26 +2,30 @@
 
 #include "../SubSeqVector.h"
 
-TEST(SubSeqVectorTest, Constructor) {
-   SubSeqVector<int> vec;
-   EXPECT_EQ(0U, vec.size());
-}
+TEST(SubSeqVectorTest, Simple) {
+   double vals[] = {2.2, 3.4, 5.1, 7.7, 11.6};
 
-TEST(SubSeqVectorTest, Destructor) {
-   static int numDestructed = 0;
+   SubSeqVector<double> test;
+   EXPECT_EQ(0U, test.size());
 
-   struct Foo {
-      Foo() { }
-      ~Foo() { numDestructed++ }
-   };
+   SubSeqVector<double> v1(vals, vals+5);
+   EXPECT_EQ(5U, v1.size());
+   EXPECT_EQ(2.2, v1[0]);
+   EXPECT_EQ(3.4, v1[1]);
+   EXPECT_EQ(5.1, v1[2]);
+   EXPECT_EQ(7.7, v1[3]);
+   EXPECT_EQ(11.6, v1[4]);
 
-   Foo foo1, foo2, foo3;
+   SubSeqVector<double> v2(v1.begin(), v1.end());
+   EXPECT_EQ(5U, v2.size());
+   for (int i = 0; i < 5; ++i)
+      EXPECT_EQ(v1[i], v2[i]);
 
-   SubSeqVector<Foo> vec;
-   vec.push_back(foo1);
-   vec.push_back(foo2);
-   vec.push_back(foo3);
+   //SubSeqVector v3 = v1.SubSeq(3, 4);
+   //EXPECT_EQ(1U, v3.size());
+   //EXPECT_EQ(7.7, v3[0]);
 
-   ~vec();
-   EXPECT_EQ(3, numDestructed);
+   EXPECT_EQ(7.7, v1.SubSeq(3, 4)[0]);
+   EXPECT_EQ(3.4, *v2.SubSeq(1, 3).SubSeq(0, 1).begin());
+   EXPECT_EQ(11.6, *(v2.SubSeq(1, 5).end()-1));
 }
