6aa609b581bda5760694f59e438c13f2caf3bc45
Mitchell Rosen <mitchellwrosen@gmail.com>
2013-08-27 03:33:36 +0000

    changes

--- milestone2/ShortTests/RunTests.sh
+++ milestone2/ShortTests/RunTests.sh
@@ -3,6 +3,6 @@
 for i in {1..6}; do
    ../MyGalaxySimulation < Test$i.in > Test$i.my.out
    ../GalaxySimulation < Test$i.in > Test$i.out
-   vimdiff Test$i.my.out Test$i.out
+   diff Test$i.my.out Test$i.out
    rm Test$i.my.out Test$i.out
 done
